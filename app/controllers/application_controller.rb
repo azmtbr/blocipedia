91a2851c3f7b58fd42b980fb6440c748085a885e
Jon Malis <jmalis@live.com>
2015-09-30 10:08:15 -0700

    pundit auth finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-   include Pundit 
+   include Pundit
 
    protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # after_action :verify_authorized, except: :index
+  #  unless devise_controller?
+  #    after_action :verify_policy_scoped, only: :index
+  #  end
+
+   rescue_from Pundit::NotAuthorizedError do |exception|
+     redirect_to root_url, alert: exception.message
+   end
 
 
    private
